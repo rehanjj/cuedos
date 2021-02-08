5cb6386b0c75858dceca27f3906843dca856aa42
rehanjj <78727856+rehanjj@users.noreply.github.com>
2021-02-08 10:11:22 +0000

    Get CPU clock speed

---
+++ pi/cpuspeed.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get CPU clock speed
+# rehanj@etherzine.com
+
+for src in arm core h264 isp v3d uart pwm emmc pixel vec hdmi dpi; do
+
+	echo -e "$src:\t$(vcgencmd measure_clock $src)"
+
+done
